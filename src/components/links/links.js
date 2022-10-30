6788166136206cd1dbd057d7ecccd23932e5a492
Olumuyiwa Famuagun <olumuyiwafumbi@gmail.com>
2022-10-30 20:18:18 +0100

    Updated the components

--- src/components/links/links.js
+++ src/components/links/links.js
@@ -6,24 +6,12 @@ import style from './links.module.css';
 const Links = () => {
   return (
     <div id={style.main}>
-      <div>
         <ExternalLink href='https://twitter.com' target="_blank" id={style.twitter_link}>Twitter Link</ExternalLink>
-      </div>
-      <div>
         <ExternalLink href='https://training.zuri.team/' target="_blank" id={style.btn_zuri}>Zuri Team</ExternalLink>
-      </div>
-      <div>
         <ExternalLink href='https://books.zuri.team' target="_blank" id={style.books}>Zuri Books</ExternalLink>
-      </div>
-      <div>
         <ExternalLink href='https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>' target="_blank" id={style.book_python}>Python Books</ExternalLink>
-      </div>
-      <div>
         <ExternalLink href='https://background.zuri.team' target="_blank" id={style.pitch}>Background Check for Coders</ExternalLink>
-      </div>
-      <div>
         <ExternalLink href='https://books.zuri.team/design-rules' target="_blank" id={style.book_design}>Design Books</ExternalLink>
-      </div>
     </div>
   )
 }
