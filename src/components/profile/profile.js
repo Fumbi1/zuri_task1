2636153c53eaefb7544d5ec4e43f352135842b55
Olumuyiwa Famuagun <olumuyiwafumbi@gmail.com>
2022-10-30 18:40:02 +0100

    Updated all components

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RiShareForwardLine } from 'react-icons/ri';
+import { CiCircleMore } from 'react-icons/ci'
 import style from './profile.module.css';
 import profile from '../../assets/profile__img.png';
 
@@ -8,16 +9,16 @@ const Profile = () => {
     <div id={style.mainn}>
         <div className={style.main}>
           <div className={style.profile}>
-              <img src={profile} alt='profile-pics' id={style.profile_img}/>
-              <h1 id={style.twitter}>Olumuyiwa Fumbi</h1>
-              <h1 id={style.slack}>Muyiwa</h1>
+            <img src={profile} alt='profile-pics' id={style.profile_img}/>
+            <h1 id={style.twitter}>Olumuyiwa Fumbi</h1>
+            <h1 id={style.slack}>Muyiwa</h1>
 
-              <div className={style.share}>
+            <div className={style.share}>
               <RiShareForwardLine />
-              </div>
-              <div className={style.ellipsis}>
-                  
-              </div>
+            </div>
+            <div className={style.ellipsis}>
+              <CiCircleMore />
+            </div>
           </div>
         </div>
     </div>
