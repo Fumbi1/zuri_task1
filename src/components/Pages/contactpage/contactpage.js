2fac4d019aa410e6a67c2c0990a6cea8bd49df31
Fumbi1 <olumuyiwafumbi@gmail.com>
2022-11-05 18:28:04 +0100

    Updated the Contact page styles
    Completed the styles and responsiveness

--- src/components/Pages/contactpage/contactpage.js
+++ src/components/Pages/contactpage/contactpage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './contactpage.css'
+import Footer from '../../footer/footer';
 
 
 const Contactpage = () => {
@@ -10,39 +11,39 @@ const Contactpage = () => {
   }
 
   return (
-    <form>
-    <p id='contact_me'>Contact Me</p>
-    <p id='contact_desc'>Hi there, contact me to ask me about anything you have in mind.</p>
+    <div id='form_section'>
+      <form>
+        <p id='contact_me'>Contact Me</p>
+        <p id='contact_desc'>Hi there, contact me to ask me about anything you have in mind.</p>
+        <div id='name_section'>
+          <div>
+            <label for='first_name' className='name_label'>First name</label><br/>
+            <input type='text' placeholder='Enter your first name' id='first_name'/>
+          </div>
+          <div>
+            <label for='last_name' className='name_label'>Last name</label><br/>
+            <input type='text' placeholder='Enter your last name' id='last_name' />
+          </div>
+        </div>
+        <div id='email_section'>
+          <label for='email' className='name_label'>Email</label><br/>
+          <input type='email' placeholder='yourname@email.com' id='email' />
+        </div>
+        <div id='message_section'>
+          <label for='message' className='name_label'>Message</label><br/>
+          <textarea placeholder="Send me a message and I'll reply you as soon as possible..." id='message' />
+        </div>
+        
+        <div id='radio_section'>
+          <input type='radio' id='radio' onClick={handleClick} checked=
+          {radioState} />
+          <label for='radio' id='radio_label'>You agree to providing your data to <span>Olumuyiwa Famuagun</span> who may contact you.</label>
+        </div>
+        <button disabled={!radioState} id='btn__submit'>Send message</button>
+      </form>
 
-    <div id='name_section'>
-      <div>
-        <label for='first_name' className='name_label'>First name</label><br/>
-        <input type='text' placeholder='Enter your first name' id='first_name' />
-      </div>
-      <div>
-        <label for='last_name' className='name_label'>Last name</label><br/>
-        <input type='text' placeholder='Enter your last name' id='last_name' />
-      </div>
+      <Footer />
     </div>
-
-    <div id='email_section'>
-      <label for='email' className='name_label'>Email</label><br/>
-      <input type='email' placeholder='yourname@email.com' id='email' />
-    </div>
-
-    <div id='message_section'>
-      <label for='message' className='name_label'>Message</label><br/>
-      <textarea placeholder="Send me a message and I'll reply you as soon as possible..." id='message' />
-    </div>
-    
-    <div>
-      <input type='radio' id='radio' onClick={handleClick} checked=
-      {radioState} />
-      <label for='radio' id='radio_label'>You agree to providing your data to <span>Olumuyiwa Famuagun</span> who may contact you.</label>
-    </div>
-
-    <button disabled={!radioState} onClick={()=> console.log("Hellow")} id='btn__submit'>Send message</button>
-  </form>
   )
 }
 
